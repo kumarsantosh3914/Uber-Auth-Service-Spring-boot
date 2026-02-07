612027e74485fed538ed7ea9655c911aee1ab534
Santosh Kumar <santoshkumar15novmth@gmail.com>
2026-02-08 00:32:08 +0530

    Added signin api

--- src/main/java/org/example/uberauthservice/controllers/AuthController.java
+++ src/main/java/org/example/uberauthservice/controllers/AuthController.java
@@ -1,17 +1,24 @@
 package org.example.uberauthservice.controllers;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.uberauthservice.dtos.AuthRequestDto;
+import org.example.uberauthservice.dtos.AuthResponseDto;
 import org.example.uberauthservice.dtos.PassengerDto;
 import org.example.uberauthservice.dtos.PassengerSignupRequestDto;
 import org.example.uberauthservice.services.AuthService;
 import org.example.uberauthservice.services.JwtService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -34,5 +41,34 @@ public class AuthController {
     public ResponseEntity<PassengerDto> signUp(@RequestBody PassengerSignupRequestDto passengerSignupRequestDto) {
         PassengerDto response = authService.signupPassenger(passengerSignupRequestDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin/passenger")
+    public ResponseEntity<?> signIn(@RequestBody AuthRequestDto authRequestDto, HttpServletResponse response) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDto.getEmail(), authRequestDto.getPassword()));
+        if(authentication.isAuthenticated()) {
+            String jwtToken = jwtService.createToken(authRequestDto.getEmail());
+
+            ResponseCookie cookie = ResponseCookie.from("JwtToken", jwtToken)
+                    .httpOnly(true)
+                    .secure(true)
+                    .path("/")
+                    .maxAge(7*24*3600)
+                    .build();
+
+            response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+            return new ResponseEntity<>(AuthResponseDto.builder().success(true).build(), HttpStatus.OK);
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<?> validate(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("Inside validate controller");
+        for(Cookie cookie: request.getCookies()) {
+            System.out.println(cookie.getName() + " " + cookie.getValue());
+        }
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
---
+++ src/main/java/org/example/uberauthservice/repositories/DriverRepository.java
@@ -0,0 +1,9 @@
+package org.example.uberauthservice.repositories;
+
+import org.example.uberproject.models.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
